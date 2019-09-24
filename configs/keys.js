f598ab90e7d44f071e98ef2f1f412803a68ed1a2
Stefinie fernando <minolispencer@gmail.com>
2019-09-24 22:14:13 +0530

    OAuth 2.0 configuration and implementation

---
+++ configs/keys.js
@@ -0,0 +1,15 @@
+// OAuth credentials obtained from Google API Console
+const KEYS = {
+    'googleOauth': {
+        'clientID': "948103666603-qj71gukhm51805vof2861q3huuu1uetg.apps.googleusercontent.com",
+        'clientSecret': "M61wWOyIJvJzq-3LRjQt277b",
+        'callback': '/auth/google/redirect'
+    },
+    "session_key": "secret123"
+}
+
+module.exports = KEYS
+
+
+
+
